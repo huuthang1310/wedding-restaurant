c238e0ca8a0a22a519553e42c7230915087354a9
khangnguyen33999 <kanewtf@gmail.com>
2019-10-28 11:10:31 +0700

    module 4 5 + about page
    @~@~@~@~@~@~@~@

---
+++ wp-content/themes/wedding-restaurant/about.php
@@ -0,0 +1,397 @@
+<?php /* Template Name: About */ ?>
+<!DOCTYPE html>
+<!-- saved from url=(0014)about:internet -->
+<html lang="zxx"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<title>about</title>
+		<link rel="icon" href="http://templatecs.com/demo/template/deliki/images/favicon.ico">
+		<!-- Bootstrap -->
+		<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri()?>/css/bootstrap.min.css">
+		<!-- Owl-coursel -->
+		<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri()?>/css/owl.carousel.css">
+		<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri()?>/css/owl.theme.default.css">
+		<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri()?>/css/owl.transitions.css">
+		<!-- Magnific-popup -->
+		<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri()?>/css/magnific-popup.css">
+		<!-- Fonts -->
+		<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri()?>/css/font-awesome.min.css">
+		<!-- Style -->
+		<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri()?>/css/animate.css">
+		<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri()?>/css/css.css">
+		<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri()?>/css/style.css">
+		<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri()?>/css/custom.css">
+	</head>
+	<body style="overflow: visible;">
+		<div class="preloader" style="display: none;">
+			<i class="fa fa-spinner" style="display: none;"></i>
+		</div>
+		<header class="header-fixed">
+			<div class="main-header bg-f">
+				<div class="container">
+					<div class="row align-items-center">
+						<div class="col-6 col-md-4 col-lg-3">
+							<a href="http://templatecs.com/demo/template/deliki/html/about.html#" class="logo"><img src="<?php echo get_template_directory_uri()?>/images/logo-foody.png" alt="image"></a>
+						</div>
+						<div class="col-6 col-md-8 col-lg-7 hidden-md hidden-lg">
+							<nav id="holder_mn" class="menu-nav pull-right sub-menu-light">
+								<div class="menu-btn"></div>
+								<div class="menu-wrap">
+									<div class="menu-btn-close"></div>
+									<ul class="menu">
+										<li><a href="http://templatecs.com/demo/template/deliki/html/home.html">home</a></li>
+										<li><a href="http://templatecs.com/demo/template/deliki/html/about.html">about</a></li>
+										<li><a href="http://templatecs.com/demo/template/deliki/html/menu.html">menu</a></li>
+										<li><a href="http://templatecs.com/demo/template/deliki/html/reservations.html">reservations</a></li>
+										<li>
+											<a href="http://templatecs.com/demo/template/deliki/html/about.html#">blog</a>
+											<ul class="sub-menu">
+												<li><a href="http://templatecs.com/demo/template/deliki/html/blog_list.html">blog_list</a></li>
+												<li><a href="http://templatecs.com/demo/template/deliki/html/single_blog.html">single_blog</a></li>
+											</ul>
+										</li>
+										<li>
+											<a href="http://templatecs.com/demo/template/deliki/html/about.html#">pages</a>
+											<ul class="sub-menu">
+												<li><a href="http://templatecs.com/demo/template/deliki/html/error_404.html">error_404</a></li>
+												<li><a href="http://templatecs.com/demo/template/deliki/html/contact.html">contact</a></li>
+											</ul>
+										</li>
+									</ul>
+								</div>
+							</nav>
+						</div>
+						<div class="col-lg-2 d-none d-lg-block">
+							<a href="http://templatecs.com/demo/template/deliki/html/about.html#" class="btn float-right">Book a table</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</header>
+		<div id="wrap-body">
+			<section class="page-title">
+				<div class="card bg-offset bg-fixed h250" data-image-src="<?php echo get_template_directory_uri()?>/images/kawin-haraffsai-75421.jpg" style="background-image: url(&quot;<?php echo get_template_directory_uri()?>/images/kawin-haraffsai-75421.jpg&quot;);">
+					<div class="card-img-overlay">
+						<div class="container">
+							<h1>about</h1>
+						</div>
+					</div>
+				</div>
+			</section>
+			<section data-image-src="<?php echo get_template_directory_uri()?>/images/food-3327977_960_720.png" class="bg-offset bg-size-70 p-t-0" style="background-image: url(&quot;<?php echo get_template_directory_uri()?>/images/food-3327977_960_720.png&quot;);">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-7 col-lg-6">
+							<div class="card card-5x m-t-150 bg-f p-40 p-xs-30 relative box-shadow-sm">
+								<div class="card-body">
+									<h2 class="card-title">About Deli<strong class="main-color">ki</strong> </h2>
+									<h4>Lorem ipsum dolor sit amet consectetur adipisicing  labore edolore</h4>
+									<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitationipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit</p>
+									<div>
+										<img class="card-img-top w180 m-t-30" src="<?php echo get_template_directory_uri()?>/images/mt-0951-text-img.png" alt="image">
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-5 col-lg-4 f-right">
+							<div class="p-20 bg-f5 m-t-md-50 text-center">
+								<div class="bg-f p-30 quote-after">
+									<img src="<?php echo get_template_directory_uri()?>/images/us27.jpg" alt="image" class="w150 rad-100">
+									<p class="m-t-20 italic line-default"> <i class="fa fa-quote-left m-r-5 f-30 color-9"></i> Lorem ipsum dolor sit amet consecsed d do eiusmod temp incidunt olore magna aliqu Lorem ipsum dolor sit amet et dincidunt ut laboliqu</p>
+									<h6 class="f-16 f-bold">Jan Bold</h6>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+			<section class="cards cards-5x2 icns-round-100 color-3 m-0 bg-offset" data-image-src="<?php echo get_template_directory_uri()?>/images/food-2863553_960_720.jpg" style="background-image: url(&quot;<?php echo get_template_directory_uri()?>/images/food-2863553_960_720.jpg&quot;);">
+				<div class="container">
+					<div class="block-title">
+						<h4 class="card-subtitle">What we do</h4>
+						<h2 class="line-default">Our Service</h2>
+					</div>
+					<div class="row row-15">
+						<div class="col-md-4">
+							<div class="card box-shadow-sm border-1x">
+								<div class="icn icn-70 border-dotted card-icon">
+									<i class="fa fa-cutlery"></i>
+								</div>
+								<div class="card-body p-t-20">
+									<h3 class="card-title">
+										<a href="http://templatecs.com/demo/template/deliki/html/about.html#">Food &amp; Wine</a>
+									</h3>
+									<p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing  labore etan dolmagna aliqua uat veniama icingmod</p>
+									<a href="http://templatecs.com/demo/template/deliki/html/about.html#" class="btn">read more</a>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="card main-bg box-shadow-sm color-f">
+								<div class="icn icn-70 border-dotted card-icon">
+									<i class="fa fa-beer"></i>
+								</div>
+								<div class="card-body p-t-20">
+									<h3 class="card-title">
+										<a href="http://templatecs.com/demo/template/deliki/html/about.html#">Beer Club</a>
+									</h3>
+									<p class="card-text color-f">Lorem ipsum dolor sit amet consectetur adipisicing  labore edolore magna aliqua uat veniama icimod</p>
+									<a href="http://templatecs.com/demo/template/deliki/html/about.html#" class="btn bg-f">read more</a>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="card box-shadow-sm border-1x">
+								<div class="icn icn-70 border-dotted card-icon">
+									<i class="fa fa-bullhorn"></i>
+								</div>
+								<div class="card-body p-t-20">
+									<h3 class="card-title">
+										<a href="http://templatecs.com/demo/template/deliki/html/about.html#">Event &amp; Wedding</a>
+									</h3>
+									<p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing  labore etan dol magna aliqua uat veniama eiusmod</p>
+									<a href="http://templatecs.com/demo/template/deliki/html/about.html#" class="btn">read more</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+			<section class="cards team cards-img-round text-center">
+				<div class="container">
+					<div class="block-title">
+						<h4 class="card-subtitle">Our chef</h4>
+						<h2 class="line-default">meet our team</h2>
+					</div>
+					<div class="owl-carousel owl-theme owl-loaded owl-drag owl-theme-1 owl-nav-1 m-b-ab-5" data-itemsdesktop="4" data-itemsdesktopsmall="3" data-itemstablet="2" data-itemsmobile="1" data-nav="false" data-margin="60" data-dots="false" data-center="false" data-darg="true">
+						<div class="owl-stage-outer">
+							<div class="owl-stage" style="width: 1230px; transform: translate3d(0px, 0px, 0px); transition: all 0s ease 0s;">
+								<div class="owl-item active" style="width: 247.5px; margin-right: 60px;">
+									<div class="card">
+										<img src="<?php echo get_template_directory_uri()?>/images/chef1.jpg" alt="image">
+										<div class="card-body">
+											<h4 class="card-title">
+												<a href="http://templatecs.com/demo/template/deliki/html/about.html#">David Behan</a>
+											</h4>
+											<p>chef</p>
+										</div>
+									</div>
+								</div>
+								<div class="owl-item active" style="width: 247.5px; margin-right: 60px;">
+									<div class="card">
+										<img src="<?php echo get_template_directory_uri()?>/images/chef2.jpg" alt="image">
+										<div class="card-body">
+											<h4 class="card-title">
+												<a href="http://templatecs.com/demo/template/deliki/html/about.html#">Mark jone</a>
+											</h4>
+											<p>chef</p>
+										</div>
+									</div>
+								</div>
+								<div class="owl-item active" style="width: 247.5px; margin-right: 60px;">
+									<div class="card">
+										<img src="<?php echo get_template_directory_uri()?>/images/chef5.jpg" alt="image">
+										<div class="card-body">
+											<h4 class="card-title">
+												<a href="http://templatecs.com/demo/template/deliki/html/about.html#">Mario Garcia</a>
+											</h4>
+											<p>chef</p>
+										</div>
+									</div>
+								</div>
+								<div class="owl-item active" style="width: 247.5px; margin-right: 60px;">
+									<div class="card">
+										<img src="<?php echo get_template_directory_uri()?>/images/chef3.jpg" alt="image">
+										<div class="card-body">
+											<h4 class="card-title">
+												<a href="http://templatecs.com/demo/template/deliki/html/about.html#">Chiris whitten</a>
+											</h4>
+											<p>chef</p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						
+						
+					<div class="owl-nav disabled"><button type="button" role="presentation" class="owl-prev"><span aria-label="Previous">‹</span></button><button type="button" role="presentation" class="owl-next"><span aria-label="Next">›</span></button></div><div class="owl-dots disabled"></div></div>
+				</div>
+			</section>
+			<section class="cards statistic bg-offset color-3 cards-title-40" data-image-src="<?php echo get_template_directory_uri()?>/images/food-2863553_960_720.jpg" style="background-image: url(&quot;<?php echo get_template_directory_uri()?>/images/food-2863553_960_720.jpg&quot;);">
+				<div class="container">
+					<div class="row row-15">
+						<div class="col-sm-6 col-md-3">
+							<div class="card">
+								<i class="fa fa-heart main-color m-b-20"></i>
+								<div class="card-body">
+									<h4 class="card-title">1460</h4>
+									<p>Clients Happy</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-sm-6 col-md-3">
+							<div class="card">
+								<i class="fa fa-cutlery main-color m-b-20"></i>
+								<div class="card-body">
+									<h4 class="card-title">156</h4>
+									<p>food</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-sm-6 col-md-3">
+							<div class="card">
+								<i class="fa fa-group main-color m-b-20"></i>
+								<div class="card-body">
+									<h4 class="card-title">007</h4>
+									<p>chef</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-sm-6 col-md-3">
+							<div class="card">
+								<i class="fa fa-bell main-color m-b-20"></i>
+								<div class="card-body">
+									<h4 class="card-title">012</h4>
+									<p>Awards</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+			<section class="cards">
+				<div class="container">
+					<div class="block-title">
+						<h4 class="card-subtitle">Our blog</h4>
+						<h2 class="line-default">Featured Press</h2>
+					</div>
+					<div class="row row-15">
+						<div class="col-lg-6">
+							<div class="card card-inline">
+								<div class="row">
+									<div class="col-sm-6">
+										<img src="<?php echo get_template_directory_uri()?>/images/seafood-2187507_960_720.jpg" alt="image">
+									</div>
+									<div class="col-sm-6">
+										<div class="card-body m-t-xs-30">
+											<div class="card-date-1">
+												<div class="card-date-inner">
+													<h6 class="main-color">12</h6>
+													<p>jannuary</p>
+												</div>
+											</div>
+											<h3 class="card-title">
+												<a href="http://templatecs.com/demo/template/deliki/html/about.html#">Mountain mike’s pizza opens new location in redwood city</a>
+											</h3>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-6">
+							<div class="card card-inline m-b-0">
+								<div class="row">
+									<div class="col-sm-6">
+										<img src="<?php echo get_template_directory_uri()?>/images/schnitzel-1817337_960_720.jpg" alt="image">
+									</div>
+									<div class="col-sm-6">
+										<div class="card-body m-t-xs-30">
+											<div class="card-date-1">
+												<div class="card-date-inner">
+													<h6 class="main-color">04</h6>
+													<p>jannuary</p>
+												</div>
+											</div>
+											<h3 class="card-title">
+												<a href="http://templatecs.com/demo/template/deliki/html/about.html#">Corner bakery is now offering fresh, wholesome seasonal specials</a>
+											</h3>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+		</div>
+		<footer class="block">
+			<div class="footer-top color-f">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-4">
+							<img src="<?php echo get_template_directory_uri()?>/images/logo-fooddy-w.png" alt="image" class="m-b-25">
+							<p class="m-b-10">1418 Riverwood Drive, Suite 3845 Cottonwood</p>
+							<p class="m-b-10">0123 456 789 - 0987 555 666</p>
+							<p class="m-b-10">food@gmail.com</p>
+						</div>
+						<div class="col-md-4">
+							<h4 class="title">Newsletter</h4>
+							<form class="m-t-5 form-lg">
+								<div class="form-group">
+									<input type="text" class="form-control no-border" placeholder="Enter your email">
+								</div>
+								<button type="submit" class="btn m-t-0">Subscribe</button>
+							</form>
+						</div>
+						<div class="col-md-3 f-right">
+							<h4 class="title">gallery</h4>
+							<div class="row row-3">
+								<div class="col-4">
+									<div class="card">
+										<img src="<?php echo get_template_directory_uri()?>/images/restaurant-939434_960_720.jpg" alt="image">
+									</div>
+								</div>
+								<div class="col-4">
+									<div class="card">
+										<img src="<?php echo get_template_directory_uri()?>/images/seafood-2187507_960_720.jpg" alt="image">
+									</div>
+								</div>
+								<div class="col-4">
+									<div class="card">
+										<img src="<?php echo get_template_directory_uri()?>/images/stefan-johnson-124186.jpg" alt="image">
+									</div>
+								</div>
+								<div class="col-4">
+									<div class="card">
+										<img src="<?php echo get_template_directory_uri()?>/images/salad-2068210_960_720.jpg" alt="image">
+									</div>
+								</div>
+								<div class="col-4">
+									<div class="card">
+										<img src="<?php echo get_template_directory_uri()?>/images/restaurant-1092728_960_720.jpg" alt="image">
+									</div>
+								</div>
+								<div class="col-4">
+									<div class="card">
+										<img src="<?php echo get_template_directory_uri()?>/images/webvilla-293.jpg" alt="image">
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="bg-0">
+				<div class="container">
+					<div class="row">
+						<div class="col-sm-6">
+							<p class="text-uppercase f-11 p-t-15 p-b-15">2019 Designed by <a href="http://templatecs.com/demo/template/deliki/html/about.html#" class="f-11">them3x</a> . All rights reserved</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
+		<!-- jQuery -->
+		<script src="<?php echo get_template_directory_uri()?>/js/jquery-2.2.4.min.js"></script>
+		<!-- Bootstrap -->
+		<script src="<?php echo get_template_directory_uri()?>/js/popper.min.js"></script>
+		<script src="<?php echo get_template_directory_uri()?>/js/bootstrap.min.js"></script>
+		<!-- Magnific-popup -->
+		<script src="<?php echo get_template_directory_uri()?>/js/jquery.magnific-popup.min.js"></script>
+		<!-- Owl-coursel -->
+		<script src="<?php echo get_template_directory_uri()?>/js/owl.carousel.min.js"></script>
+		<!-- Script -->
+		<script src="<?php echo get_template_directory_uri()?>/js/script.js"></script>
+	
+</body></html>
---
+++ wp-content/themes/wedding-restaurant/module/module-4.php
@@ -0,0 +1,58 @@
+<div class="container">
+					<div class="bg-offset bg-size-80" data-image-src="<?php echo get_template_directory_uri()?>/images/food-3327977_960_720.png" style="background-image: url(&quot;<?php echo get_template_directory_uri()?>/images/food-3327977_960_720.png&quot;);">
+						<div class="row">
+							<div class="col-md-8 col-lg-6 f-right">
+								<div class="bg-f p-40 p-xs-20 card box-shadow-sm border-1x">
+									<div class="card-body">
+										<div>
+											<div class="block-title m-b-50">
+												<h4 class="card-subtitle">Sale up to 20%</h4>
+												<h2 class="line-default f-30">week's special</h2>
+											</div>
+											<div class="menu-food">
+												<h4 class="d-flex">
+													<a href="http://templatecs.com/demo/template/deliki/html/home.html#">Tomato Bruscgetta</a>
+													<span class="col"></span>
+													<span>$9.00</span>
+												</h4>
+												<p>Tomatoes, Olive Oil, Cheese</p>
+											</div>
+											<div class="menu-food">
+												<h4 class="d-flex">
+													<a href="http://templatecs.com/demo/template/deliki/html/home.html#">Gnocchi con speck</a>
+													<span class="col"></span>
+													<span>$9.00</span>
+												</h4>
+												<p>Tomatoes, Olive Oil, Cheese</p>
+											</div>
+											<div class="menu-food">
+												<h4 class="d-flex">
+													<a href="http://templatecs.com/demo/template/deliki/html/home.html#">Crema di funghi</a>
+													<span class="col"></span>
+													<span>$9.00</span>
+												</h4>
+												<p>Tomatoes, Olive Oil, Cheese</p>
+											</div>
+											<div class="menu-food">
+												<h4 class="d-flex">
+													<a href="http://templatecs.com/demo/template/deliki/html/home.html#">Tomato salad</a>
+													<span class="col"></span>
+													<span>$9.00</span>
+												</h4>
+												<p>Tomatoes, Olive Oil, Cheese</p>
+											</div>
+											<div class="menu-food">
+												<h4 class="d-flex">
+													<a href="http://templatecs.com/demo/template/deliki/html/home.html#">Cheese burger</a>
+													<span class="col"></span>
+													<span>$9.00</span>
+												</h4>
+												<p>Tomatoes, Olive Oil, Cheese</p>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
