77f73701494a95600211d1505d7c4e664e6af35c
huuthang1310 <buihuuthang199@gmail.com>
2019-10-28 11:03:16 +0700

    module 1 2 3

--- wp-content/themes/wedding-restaurant/module/module-1.php
+++ wp-content/themes/wedding-restaurant/module/module-1.php
@@ -1,15 +1,54 @@
-<section class="banner p-0">
-				<div class="card bg-offset card-5x" data-image-src="../images/kawin-haraffsai-75421.jpg" style="background-image: url(&quot;../images/kawin-haraffsai-75421.jpg&quot;);">
-					<div></div>
-					<div class="card-img-overlay">
-						<div class="container">
-							<div class="max-w-700">
-								<h4 class="card-subtitle f-30 color-f f-sm-25 m-b-15">Save up to 25%</h4>
-								<h1 class="card-title f-sm-40 f-xs-35 f-weight-900">Delicious <strong class="main-color">Food</strong> </h1>
-								<h6 class="card-text max-w-800 f-20 f-sm-13">Breadfast - Luch - Dinner - Dersert - Wine</h6>
-								<a href="http://templatecs.com/demo/template/deliki/html/home.html#" class="btn btn-lg m-t-50">discovery menu</a>
-							</div>
-						</div>
-					</div>
-				</div>
-			</section>
+<header class="header-fixed smaller">
+    <div class="main-header bg-f">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-6 col-md-4 col-lg-3">
+                    <a href="http://templatecs.com/demo/template/deliki/html/home.html#" class="logo"><img
+                            src="<?php echo get_template_directory_uri() ?>/images/logo-foody.png" alt="image"></a>
+                </div>
+                <div class="col-6 col-md-8 col-lg-7 hidden-md hidden-lg">
+                    <nav id="holder_mn" class="menu-nav pull-right sub-menu-light">
+                        <div class="menu-btn"></div>
+                        <div class="menu-wrap">
+                            <div class="menu-btn-close"></div>
+                            <ul class="menu">
+                                <li><a href="http://templatecs.com/demo/template/deliki/html/home.html">home</a></li>
+                                <li><a href="http://templatecs.com/demo/template/deliki/html/about.html">about</a></li>
+                                <li><a href="http://templatecs.com/demo/template/deliki/html/menu.html">menu</a></li>
+                                <li><a
+                                        href="http://templatecs.com/demo/template/deliki/html/reservations.html">reservations</a>
+                                </li>
+                                <li>
+                                    <a href="http://templatecs.com/demo/template/deliki/html/home.html#">blog</a>
+                                    <ul class="sub-menu">
+                                        <li><a
+                                                href="http://templatecs.com/demo/template/deliki/html/blog_list.html">blog_list</a>
+                                        </li>
+                                        <li><a
+                                                href="http://templatecs.com/demo/template/deliki/html/single_blog.html">single_blog</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <a href="http://templatecs.com/demo/template/deliki/html/home.html#">pages</a>
+                                    <ul class="sub-menu">
+                                        <li><a
+                                                href="http://templatecs.com/demo/template/deliki/html/error_404.html">error_404</a>
+                                        </li>
+                                        <li><a
+                                                href="http://templatecs.com/demo/template/deliki/html/contact.html">contact</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+                <div class="col-lg-2 d-none d-lg-block">
+                    <a href="http://templatecs.com/demo/template/deliki/html/home.html#" class="btn float-right">Book a
+                        table</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
