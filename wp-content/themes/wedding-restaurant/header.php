1b010f795e64b85bcc41d9623829147b3e5b44fa
huuthang1310 <buihuuthang199@gmail.com>
2019-10-28 10:04:26 +0700

    add lại project

--- wp-content/themes/wedding-restaurant/header.php
+++ wp-content/themes/wedding-restaurant/header.php
@@ -1,86 +1,58 @@
-<?php $Url2 = get_template_directory_uri(); ?>
-<!DOCTYPE html>
-<html <?php language_attributes(); ?>class="no-js">
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Wedding_Restaurant
+ */
 
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
 <head>
-    <meta charset="<?php bloginfo('charset') ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
 
-    <title>
-        <?php 
-        wp_title('|', true, 'right');
-        bloginfo( 'name' );
-    ?>
-    </title>
-    <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
-    <?php if ( is_singular() && pings_open( get_queried_object() ) ) : ?>
-    <link rel="pingback" href="<?php echo esc_url( get_bloginfo( 'pingback_url' ) ); ?>">
-    <?php endif; ?>
-    <?php wp_head(); ?>
-    <link rel='stylesheet'
-        href='https://fonts.googleapis.com/css?family=Montserrat:100,100italic,200,200italic,300,300italic,regular,italic,500,500italic,600,600italic,700,700italic,800,800italic,900,900italic|Open+Sans:300,300italic,regular,italic,600,600italic,700,700italic,800,800italic|Asap:regular,italic,500,500italic,600,600italic,700,700italic&#038;subset=latin-ext,latin,vietnamese,cyrillic-ext,greek,greek-ext,cyrillic&#038;ver=4.9.5'
-        type='text/css' media='all'>
+	<?php wp_head(); ?>
 </head>
 
-<body
-    class="home page-template-default page page-id-2039 gdlr-core-body financity-body financity-body-front financity-full  financity-with-sticky-navigation gdlr-core-link-to-lightbox">
-    <div class="financity-mobile-header-wrap">
-        <div class="financity-mobile-header financity-header-background financity-style-slide"
-            id="financity-mobile-header">
-            <div class="financity-mobile-header-container financity-container">
-                <div class="financity-logo  financity-item-pdlr">
-                    <div class="financity-logo-inner">
-                        <a href="<?php echo get_site_url(); ?>"><img
-                                src="<?php echo $Url2 ?>/upload/logo.png"
-                                alt="" width="211" height="48"></a>
-                    </div>
-                </div>
-                <div class="financity-mobile-menu-right">
-                    <div class="financity-mobile-menu"><a
-                            class="financity-mm-menu-button financity-mobile-menu-button financity-mobile-button-hamburger"
-                            href="#financity-mobile-menu"><span></span></a>
-                        <div class="financity-mm-menu-wrap financity-navigation-font" id="financity-mobile-menu"
-                            data-slide="right">
-                            <ul id="menu-main-navigation" class="m-menu">
-                                <li
-                                    class="menu-item menu-item-home current-menu-item page_item page-item-2039 current_page_item">
-                                    <a href="/">Home</a></li>
-                                <li class="menu-item menu-item-has-children"><a href="<?php echo get_site_url(); ?>/services">Our Services</a>
-                                </li>
-                                <li class="menu-item menu-item-has-children"><a href="<?php echo get_site_url(); ?>/learnmore">Learn More</a>
-                                </li>
-                                <li class="menu-item menu-item-has-children"><a href="<?php echo get_site_url(); ?>/about/#dedicated">Dedicated Account Provider</a>
-                                </li>
-                                <li class="menu-item menu-item-has-children"><a href="<?php echo get_site_url(); ?>/about/#compliance">Legal Compliance</a>
-                                </li>
-                                <li class="menu-item menu-item-has-children"><a
-                                        href="<?php echo get_site_url(); ?>/About">About Us</a>
-                                </li>
-                                <li class="menu-item menu-item-has-children"><a href="<?php echo get_site_url(); ?>/ContactUs">Contact</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="financity-body-outer-wrapper ">
-        <div class="financity-body-wrapper clearfix  financity-with-transparent-navigation financity-with-frame">
-            <div class="financity-header-background-transparent">
-                <header class="financity-header-wrap financity-header-style-bar  financity-style-left">
-                    <div class="financity-header-background"></div>
-                    <div class="financity-header-container clearfix  financity-container">
-                        <div class="financity-header-container-inner">
-                            <div class="financity-logo  financity-item-pdlr">
-                                <div class="financity-logo-inner">
-                                    <a href="<?php echo get_site_url(); ?>"><img
-                                            src="<?php echo $Url2 ?>/images/logo.png"
-                                            alt=""></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <?php get_template_part( 'module/module-1', 'menu' ); ?>
-                </header>
-            </div>
+<body <?php body_class(); ?>>
+<div id="page" class="site">
+	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'wedding-restaurant' ); ?></a>
+
+	<header id="masthead" class="site-header">
+		<div class="site-branding">
+			<?php
+			the_custom_logo();
+			if ( is_front_page() && is_home() ) :
+				?>
+				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<?php
+			else :
+				?>
+				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+				<?php
+			endif;
+			$wedding_restaurant_description = get_bloginfo( 'description', 'display' );
+			if ( $wedding_restaurant_description || is_customize_preview() ) :
+				?>
+				<p class="site-description"><?php echo $wedding_restaurant_description; /* WPCS: xss ok. */ ?></p>
+			<?php endif; ?>
+		</div><!-- .site-branding -->
+
+		<nav id="site-navigation" class="main-navigation">
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'wedding-restaurant' ); ?></button>
+			<?php
+			wp_nav_menu( array(
+				'theme_location' => 'menu-1',
+				'menu_id'        => 'primary-menu',
+			) );
+			?>
+		</nav><!-- #site-navigation -->
+	</header><!-- #masthead -->
+
+	<div id="content" class="site-content">
