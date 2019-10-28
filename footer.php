2369ae24d5e5d0a77489087ad29f4d6d34690253
huuthang1310 <buihuuthang199@gmail.com>
2019-10-28 07:51:53 +0700

    add project

---
+++ footer.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Wedding_Restaurant
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'wedding-restaurant' ) ); ?>">
+				<?php
+				/* translators: %s: CMS name, i.e. WordPress. */
+				printf( esc_html__( 'Proudly powered by %s', 'wedding-restaurant' ), 'WordPress' );
+				?>
+			</a>
+			<span class="sep"> | </span>
+				<?php
+				/* translators: 1: Theme name, 2: Theme author. */
+				printf( esc_html__( 'Theme: %1$s by %2$s.', 'wedding-restaurant' ), 'wedding-restaurant', '<a href="http://underscores.me/">Underscores.me</a>' );
+				?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+<<<<<<< HEAD
+=======
+
+<script type='text/javascript' src='<?php echo $Url ?>/js/app.js?ver=2'></script>
+<script type='text/javascript' src='<?php echo $Url ?>/js/jquery.easing.min.js'></script>
+</body>
+>>>>>>> 36ee8428aa1ea227b77bb7197ac4c8d392117b62
+
+</body>
+</html>
