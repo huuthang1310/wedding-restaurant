79a39b0f91e7d7527e08774ed5f4d01a538a8892
Nguyễn Khang <kanewtf@gmail.com>
2019-11-02 11:30:26 +0700

    module 16 17 18
    module 17 - 18 = module 17

---
+++ wp-content/themes/wedding-restaurant/module/module-17-menu/17-content.php
@@ -0,0 +1,261 @@
+<section class="cards cards-pad-40 cards-shadow-sm cards-border m-0 bg-fa">
+				<div class="container">
+					<div class="block-title">
+						<h4 class="card-subtitle">Welcome to</h4>
+						<h2 class="line-default">Discovery menu</h2>
+						<p class="max-w-800 text-center m-b-60">Lorem ipsum dolor sit amet consectetur adipisicing  labore</p>
+					</div>
+					<div class="row row-15">
+						<div class="col-md-6">
+							<div class="card">
+								<div class="card-body p-0">
+									<div class="title-menu">
+										<h3>Sea food</h3>
+									</div>
+									<div class="menu-food">
+										<h4 class="d-flex">
+											<a href="#">Boneless Buffalo Wings</a>
+											<span class="col"></span>
+											<span>$9.00</span>
+										</h4>
+										<p>Tomatoes, Olive Oil, Cheese</p>
+									</div>
+									<div class="menu-food">
+										<h4 class="d-flex">
+											<a href="#">Mozzarella Moons</a>
+											<span class="col"></span>
+											<span>$9.00</span>
+										</h4>
+										<p>Tomatoes, Olive Oil, Cheese</p>
+									</div>
+									<img src="img/seafood-2187507_960_720.jpg" alt="image" class="m-t-25">
+									<div class="menu-food">
+										<h4 class="d-flex">
+											<a href="#">Outrageous Potato Skins</a>
+											<span class="col"></span>
+											<span>$9.00</span>
+										</h4>
+										<p>Tomatoes, Olive Oil, Cheese</p>
+									</div>
+									<div class="menu-food">
+										<h4 class="d-flex">
+											<a href="#">Soup &amp; Garden Side Salad</a>
+											<span class="col"></span>
+											<span>$9.00</span>
+										</h4>
+										<p>Tomatoes, Olive Oil, Cheese</p>
+									</div>
+									<div class="menu-food">
+										<h4 class="d-flex">
+											<a href="#">BBQ Chicken Wrap</a>
+											<span class="col"></span>
+											<span>$9.00</span>
+										</h4>
+										<p>Tomatoes, Olive Oil, Cheese</p>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-6">
+							<div class="card">
+								<div class="card-body p-0">
+									<div class="title-menu">
+										<h3>Salad</h3>
+									</div>
+									<div class="menu-food">
+										<h4 class="d-flex">
+											<a href="#">Boneless Buffalo Wings</a>
+											<span class="col"></span>
+											<span>$9.00</span>
+										</h4>
+										<p>Tomatoes, Olive Oil, Cheese</p>
+									</div>
+									<img src="img/restaurant-1092728_960_720.jpg" alt="image" class="m-t-25">
+									<div class="menu-food">
+										<h4 class="d-flex">
+											<a href="#">Mozzarella Moons</a>
+											<span class="col"></span>
+											<span>$9.00</span>
+										</h4>
+										<p>Tomatoes, Olive Oil, Cheese</p>
+									</div>
+									<div class="menu-food">
+										<h4 class="d-flex">
+											<a href="#">Outrageous Potato Skins</a>
+											<span class="col"></span>
+											<span>$9.00</span>
+										</h4>
+										<p>Tomatoes, Olive Oil, Cheese</p>
+									</div>
+									<div class="menu-food">
+										<h4 class="d-flex">
+											<a href="#">Soup &amp; Garden Side Salad</a>
+											<span class="col"></span>
+											<span>$9.00</span>
+										</h4>
+										<p>Tomatoes, Olive Oil, Cheese</p>
+									</div>
+									<div class="menu-food">
+										<h4 class="d-flex">
+											<a href="#">BBQ Chicken Wrap</a>
+											<span class="col"></span>
+											<span>$9.00</span>
+										</h4>
+										<p>Tomatoes, Olive Oil, Cheese</p>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-6 col-lg-4">
+							<div class="card">
+								<div class="card-body p-0">
+									<div class="title-menu">
+										<h3>Pizza</h3>
+									</div>
+									<div class="menu-food">
+										<h4 class="d-flex">
+											<a href="#">Boneless Buffalo Wings</a>
+											<span class="col"></span>
+											<span>$9.00</span>
+										</h4>
+										<p>Tomatoes, Olive Oil, Cheese</p>
+									</div>
+									<div class="menu-food">
+										<h4 class="d-flex">
+											<a href="#">Mozzarella Moons</a>
+											<span class="col"></span>
+											<span>$9.00</span>
+										</h4>
+										<p>Tomatoes, Olive Oil, Cheese</p>
+									</div>
+									<div class="menu-food">
+										<h4 class="d-flex">
+											<a href="#">Outrageous Potato Skins</a>
+											<span class="col"></span>
+											<span>$9.00</span>
+										</h4>
+										<p>Tomatoes, Olive Oil, Cheese</p>
+									</div>
+									<div class="menu-food">
+										<h4 class="d-flex">
+											<a href="#">Soup &amp; Garden Side Salad</a>
+											<span class="col"></span>
+											<span>$9.00</span>
+										</h4>
+										<p>Tomatoes, Olive Oil, Cheese</p>
+									</div>
+									<img src="img/pizza-2000614_960_720.jpg" alt="image" class="m-t-25">
+									<div class="menu-food">
+										<h4 class="d-flex">
+											<a href="#">BBQ Chicken Wrap</a>
+											<span class="col"></span>
+											<span>$9.00</span>
+										</h4>
+										<p>Tomatoes, Olive Oil, Cheese</p>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-6 col-lg-4">
+							<div class="card">
+								<div class="card-body p-0">
+									<div class="title-menu">
+										<h3>Soup</h3>
+									</div>
+									<div class="menu-food">
+										<h4 class="d-flex">
+											<a href="#">Boneless Buffalo Wings</a>
+											<span class="col"></span>
+											<span>$9.00</span>
+										</h4>
+										<p>Tomatoes, Olive Oil, Cheese</p>
+									</div>
+									<img src="img/meal-918639_1280.jpg" alt="image" class="m-t-25">
+									<div class="menu-food">
+										<h4 class="d-flex">
+											<a href="#">Mozzarella Moons</a>
+											<span class="col"></span>
+											<span>$9.00</span>
+										</h4>
+										<p>Tomatoes, Olive Oil, Cheese</p>
+									</div>
+									<div class="menu-food">
+										<h4 class="d-flex">
+											<a href="#">Outrageous Potato Skins</a>
+											<span class="col"></span>
+											<span>$9.00</span>
+										</h4>
+										<p>Tomatoes, Olive Oil, Cheese</p>
+									</div>
+									<div class="menu-food">
+										<h4 class="d-flex">
+											<a href="#">Soup &amp; Garden Side Salad</a>
+											<span class="col"></span>
+											<span>$9.00</span>
+										</h4>
+										<p>Tomatoes, Olive Oil, Cheese</p>
+									</div>
+									<div class="menu-food">
+										<h4 class="d-flex">
+											<a href="#">BBQ Chicken Wrap</a>
+											<span class="col"></span>
+											<span>$9.00</span>
+										</h4>
+										<p>Tomatoes, Olive Oil, Cheese</p>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-6 col-lg-4">
+							<div class="card">
+								<div class="card-body p-0">
+									<div class="title-menu">
+										<h3>Desert</h3>
+									</div>
+									<div class="menu-food">
+										<h4 class="d-flex">
+											<a href="#">Boneless Buffalo Wings</a>
+											<span class="col"></span>
+											<span>$9.00</span>
+										</h4>
+										<p>Tomatoes, Olive Oil, Cheese</p>
+									</div>
+									<div class="menu-food">
+										<h4 class="d-flex">
+											<a href="#">Mozzarella Moons</a>
+											<span class="col"></span>
+											<span>$9.00</span>
+										</h4>
+										<p>Tomatoes, Olive Oil, Cheese</p>
+									</div>
+									<img src="img/cake-1868788_960_720.jpg" alt="image" class="m-t-25">
+									<div class="menu-food">
+										<h4 class="d-flex">
+											<a href="#">Outrageous Potato Skins</a>
+											<span class="col"></span>
+											<span>$9.00</span>
+										</h4>
+										<p>Tomatoes, Olive Oil, Cheese</p>
+									</div>
+									<div class="menu-food">
+										<h4 class="d-flex">
+											<a href="#">Soup &amp; Garden Side Salad</a>
+											<span class="col"></span>
+											<span>$9.00</span>
+										</h4>
+										<p>Tomatoes, Olive Oil, Cheese</p>
+									</div>
+									<div class="menu-food">
+										<h4 class="d-flex">
+											<a href="#">BBQ Chicken Wrap</a>
+											<span class="col"></span>
+											<span>$9.00</span>
+										</h4>
+										<p>Tomatoes, Olive Oil, Cheese</p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
---
+++ wp-content/themes/wedding-restaurant/module/module-18-menu/18-content.php
@@ -0,0 +1,64 @@
+<section>
+				<div class="container">
+					<div class="row">
+						<div class="col-md-5 text-center cards-pad-40 color-3 cards-title-17">
+							<div class="p-40 p-md-0">
+								<div class="card border-bottom-2x border-3 p-t-sm-0">
+									<i class="fa fa-phone color-yellow m-b-20 main-color"></i>
+									<div class="card-body">
+										<h5 class="card-title">
+											<a href="#">0123 456 789</a>
+										</h5>
+									</div>
+								</div>
+								<div class="card border-bottom-2x border-3">
+									<i class="fa fa-envelope-o color-yellow m-b-20 main-color"></i>
+									<div class="card-body">
+										<h5 class="card-title">
+											<a href="#">info@gmail.com</a>
+										</h5>
+									</div>
+								</div>
+								<div class="card">
+									<i class="fa fa-map-signs color-yellow m-b-20 main-color"></i>
+									<div class="card-body">
+										<h5 class="card-title">
+											<a href="#">1418 Riverwood Drive, Suite 3845 Cottonwood</a>
+										</h5>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-7">
+							<div class="p-30 bg-offset p-xs-0 bg-fixed m-t-sm-40" data-image-src="img/kawin-haraffsai-75421.jpg" style="background-image: url(&quot;img/kawin-haraffsai-75421.jpg&quot;);">
+								<div class="p-50 p-md-40 p-xs-0 bg-f">
+									<div class="block-title m-b-50">
+										<h4 class="card-subtitle">Book a tabe</h4>
+										<h2 class="line-default">reservations</h2>
+									</div>
+									<form class="max-w-600 form-lg">
+										<div class="form-group">
+											<input type="text" class="form-control" placeholder="Username">
+										</div>
+										<div class="form-group">
+											<input type="text" class="form-control" placeholder="Email">
+										</div>
+										<div class="form-group">
+											<input type="text" class="form-control" placeholder="Phone">
+										</div>
+										<div class="form-group">
+											<input type="text" class="form-control" placeholder="Date">
+										</div>
+										<div class="form-group">
+											<textarea class="form-control" placeholder="Messenge"></textarea>
+										</div>
+										<div class="text-center">
+											<button type="submit" class="btn btn-lg color-f">Reservation</button>
+										</div>
+									</form>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
