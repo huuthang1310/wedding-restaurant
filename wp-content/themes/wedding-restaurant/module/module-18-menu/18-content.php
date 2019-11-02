7f9582a1dfbe14a5f3a3f0a296660fb0e5d860a5
Nguyễn Khang <kanewtf@gmail.com>
2019-11-02 11:35:34 +0700

    update module 18

--- wp-content/themes/wedding-restaurant/module/module-18-menu/18-content.php
+++ wp-content/themes/wedding-restaurant/module/module-18-menu/18-content.php
@@ -1,4 +1,14 @@
-<section>
+<div id="wrap-body">
+			<section class="page-title">
+				<div class="card bg-offset bg-fixed h250" data-image-src="img/kawin-haraffsai-75421.jpg" style="background-image: url(&quot;img/kawin-haraffsai-75421.jpg&quot;);">
+					<div class="card-img-overlay">
+						<div class="container">
+							<h1>reservations</h1>
+						</div>
+					</div>
+				</div>
+			</section>
+			<section>
 				<div class="container">
 					<div class="row">
 						<div class="col-md-5 text-center cards-pad-40 color-3 cards-title-17">
@@ -62,3 +72,4 @@
 					</div>
 				</div>
 			</section>
+		</div>
