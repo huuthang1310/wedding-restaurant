601bbd28d8286d1f0a54b139c822c51683e24aee
hongnga1611 <hongngattdpt@gmail.com>
2019-11-04 00:48:50 +0700

    add module 20 + 21 + 22

--- wp-content/themes/wedding-restaurant/blog list.php
+++ wp-content/themes/wedding-restaurant/blog list.php
@@ -1,53 +1,16 @@
 <?php /* Template Name: blog */ ?>
 <?php get_header(); ?>
 		<div id="wrap-body">
-			<section class="page-title">
-				<div class="card bg-offset bg-fixed h250" data-image-src="<?php echo get_template_directory_uri()?>/images/kawin-haraffsai-75421.jpg" style="background-image: url(&quot;<?php echo get_template_directory_uri()?>/images/kawin-haraffsai-75421.jpg&quot;);">
-					<div class="card-img-overlay">
-						<div class="container">
-							<h1>blog list</h1>
-						</div>
-					</div>
-				</div>
-			</section>
+			<!--get template -->
+			<?php get_template_part( 'module/module-wp/module-22', 'blog' ); ?>
+
 			<section>
 				<div class="container">
 					<div class="row">
 						<div class="col-md-7 col-lg-8 cards cards-title-25 cards-pad-30 cards-border">
 							<div class="m-r-20 m-sm-0">
-								<div class="card card-5x">
-									<img src="<?php echo get_template_directory_uri()?>/images/pizza-2000614_960_720.jpg" alt="image">
-									<div class="card-body">
-										<h2 class="card-title">
-											<a href="http://templatecs.com/demo/template/deliki/html/blog_list.html#">Mountain mike’s pizza opens new location in redwood city</a>
-										</h2>
-										<p class="card-date">September 12, 2017</p>
-										<p>Lorem ipsum dolor sit amet consectetur adipisicing  labore et dolore magna aliqua uat veniama icing elit sed do eiusmoLorem ipsum dolor sit amet gth hjhjh jk,,consectetur adipisicing labore et dolore magnadolor sit amet consectetur adipisicing labore et dolore magna aliqua uat veniama icing elit sed do eiusmo Lorem ipsum dolor sit amet gth hjhjh jk,,consectetur adipisicing labore et dolore magna</p>
-										<a href="http://templatecs.com/demo/template/deliki/html/blog_list.html#" class="btn">read more</a>
-									</div>
-								</div>
-								<div class="card card-5x">
-									<img src="<?php echo get_template_directory_uri()?>/images/stefan-johnson-124186.jpg" alt="image">
-									<div class="card-body">
-										<h2 class="card-title">
-											<a href="http://templatecs.com/demo/template/deliki/html/blog_list.html#">Corner bakery is now offering fresh, wholesome seasonal specials</a>
-										</h2>
-										<p class="card-date">September 12, 2017</p>
-										<p>Lorem ipsum dolor sit amet consectetur adipisicing  labore et dolore magna aliqua uat veniama icing elit sed do eiusmoLorem ipsum dolor sit amet gth hjhjh jk,,consectetur adipisicing labore et dolore magnadolor sit amet consectetur adipisicing labore et dolore magna aliqua uat veniama icing elit sed do eiusmo Lorem ipsum dolor sit amet gth hjhjh jk,,consectetur adipisicing labore et dolore magna</p>
-										<a href="http://templatecs.com/demo/template/deliki/html/blog_list.html#" class="btn">read more</a>
-									</div>
-								</div>
-								<div class="card card-5x">
-									<img src="<?php echo get_template_directory_uri()?>/images/stefan-johnson-124186.jpg" alt="image">
-									<div class="card-body">
-										<h2 class="card-title">
-											<a href="http://templatecs.com/demo/template/deliki/html/blog_list.html#">Capriotti’s sandwich shop ignites 2019 with momentous growth</a>
-										</h2>
-										<p class="card-date">September 12, 2017</p>
-										<p>Lorem ipsum dolor sit amet consectetur adipisicing  labore et dolore magna aliqua uat veniama icing elit sed do eiusmoLorem ipsum dolor sit amet gth hjhjh jk,,consectetur adipisicing labore et dolore magnadolor sit amet consectetur adipisicing labore et dolore magna aliqua uat veniama icing elit sed do eiusmo Lorem ipsum dolor sit amet gth hjhjh jk,,consectetur adipisicing labore et dolore magna</p>
-										<a href="http://templatecs.com/demo/template/deliki/html/blog_list.html#" class="btn">read more</a>
-									</div>
-								</div>
+							<!--get template-->
+							<?php get_template_part( 'module/module-wp/module-20', 'blog' ); ?>
 								<nav aria-label="Page navigation example" class="m-t-20">
 									<ul class="pagination">
 										<li class="page-item"><a class="page-link" href="http://templatecs.com/demo/template/deliki/html/blog_list.html#"><i class="fa fa-angle-left"></i></a></li>
@@ -60,114 +23,9 @@
 							</div>
 						</div>
 						<div class="col-md-5 col-lg-4">
-							<div class="cards cards-padd-0 pos-sticky border-1x p-30">
-								<div class="card">
-									<div class="card-header card-header-3 p-t-0">
-										<h3>Recent post</h3>
-									</div>
-									<div class="card-body">
-										<div class="cards cards-mb-20 cards-2x medias-sm">
-											<div class="card">
-												<div class="media">
-													<img class="media-img" src="<?php echo get_template_directory_uri()?>/images/r27.jpg" alt="image">
-													<div class="media-body">
-														<h4 class="media-heading">
-															<a href="http://templatecs.com/demo/template/deliki/html/blog_list.html#">Mountain mike’s pizza opens new location in redwood city</a>
-														</h4>
-														<p class="card-date">September 12, 2017</p>
-													</div>
-												</div>
-											</div>
-											<div class="card">
-												<div class="media">
-													<img class="media-img" src="<?php echo get_template_directory_uri()?>/images/restaurant-1948732_960_720.jpg" alt="image">
-													<div class="media-body">
-														<h4 class="media-heading">
-															<a href="http://templatecs.com/demo/template/deliki/html/blog_list.html#">Corner bakery is now offering fresh, wholesome seasonal specials</a>
-														</h4>
-														<p class="card-date">September 12, 2017</p>
-													</div>
-												</div>
-											</div>
-											<div class="card">
-												<div class="media">
-													<img class="media-img" src="<?php echo get_template_directory_uri()?>/images/r24.jpg" alt="image">
-													<div class="media-body">
-														<h4 class="media-heading">
-															<a href="http://templatecs.com/demo/template/deliki/html/blog_list.html#">Capriotti’s sandwich Shop ignites 2019 with momentous growth</a>
-														</h4>
-														<p class="card-date">September 12, 2017</p>
-													</div>
-												</div>
-											</div>
-											<div class="card">
-												<div class="media">
-													<img class="media-img" src="<?php echo get_template_directory_uri()?>/images/restaurant-939434_960_720.jpg" alt="image">
-													<div class="media-body">
-														<h4 class="media-heading">
-															<a href="http://templatecs.com/demo/template/deliki/html/blog_list.html#">Get your cheese fix with fazoli’s parmesan fest</a>
-														</h4>
-														<p class="card-date">September 12, 2017</p>
-													</div>
-												</div>
-											</div>
-											<div class="card">
-												<div class="media">
-													<img class="media-img" src="<?php echo get_template_directory_uri()?>/images/restaurant-1092728_960_720.jpg" alt="image">
-													<div class="media-body">
-														<h4 class="media-heading">
-															<a href="http://templatecs.com/demo/template/deliki/html/blog_list.html#">Celebrate national burrito day at taco John’s</a>
-														</h4>
-														<p class="card-date">September 12, 2017</p>
-													</div>
-												</div>
-											</div>
-										</div>
-									</div>
-								</div>
-								<div class="card">
-									<img src="<?php echo get_template_directory_uri()?>/images/healthy-food-1553356_960_720.jpg" alt="image">
-								</div>
-								<div class="card">
-									<div class="card-header card-header-3">
-										<h3>Instagram</h3>
-									</div>
-									<div class="card-body">
-										<div class="row row-5">
-											<div class="col-sm-6">
-												<div class="card bg-e p-0">
-													<img src="<?php echo get_template_directory_uri()?>/images/baker-1620504_960_720.jpg" alt="image" class="card-top-img">
-												</div>
-											</div>
-											<div class="col-sm-6">
-												<div class="card bg-e p-0">
-													<img src="<?php echo get_template_directory_uri()?>/images/breakfast-801827_960_720.jpg" alt="image" class="card-top-img">
-												</div>
-											</div>
-											<div class="col-sm-6">
-												<div class="card bg-e p-0">
-													<img src="<?php echo get_template_directory_uri()?>/images/brownie-2342762_960_720.jpg" alt="image" class="card-top-img">
-												</div>
-											</div>
-											<div class="col-sm-6">
-												<div class="card bg-e p-0">
-													<img src="<?php echo get_template_directory_uri()?>/images/chairs-2179044_1920.jpg" alt="image" class="card-top-img">
-												</div>
-											</div>
-											<div class="col-sm-6">
-												<div class="card bg-e p-0">
-													<img src="<?php echo get_template_directory_uri()?>/images/elli-o-65548.jpg" alt="image" class="card-top-img">
-												</div>
-											</div>
-											<div class="col-sm-6">
-												<div class="card bg-e p-0">
-													<img src="<?php echo get_template_directory_uri()?>/images/burger-1015440_960_720.jpg" alt="image" class="card-top-img">
-												</div>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
+							<!--template-->
+							<?php get_template_part( 'module/module-wp/module-21', 'blog' ); ?>
+
 						</div>
 					</div>
 				</div>
