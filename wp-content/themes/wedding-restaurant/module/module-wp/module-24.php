35a36f17e3aef973dd25ce219a82304c7a913e4f
huuthang1310 <buihuuthang199@gmail.com>
2019-11-04 11:22:36 +0700

    content page

--- wp-content/themes/wedding-restaurant/module/module-wp/module-24.php
+++ wp-content/themes/wedding-restaurant/module/module-wp/module-24.php
@@ -5,17 +5,32 @@
                 <div class="m-r-20 m-r-sm-0">
                     <div class="card">
                         <div>
-                            <img src="<?php echo get_template_directory_uri() ?>/images/seafood-2187507_960_720.jpg" alt="image" class="card-top-img">
+                            <img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="<?php the_title(); ?>"
+                                 class="card-top-img">
                         </div>
                         <div class="card-body">
                             <h2 class="card-title">
-                                <a href="http://templatecs.com/demo/template/deliki/html/single_blog.html#">Mountain mike’s pizza opens new location in redwood city</a>
+                                <a
+                                    href="http://templatecs.com/demo/template/deliki/html/single_blog.html#"><?php the_title( ) ?></a>
                             </h2>
-                            <p class="card-date">September 12, 2017</p>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
-                            <img src="<?php echo get_template_directory_uri() ?>/images/schnitzel-1817337_960_720.jpg" alt="image" class="card-top-img w400 float-left m-r-30 m-b-20 m-t-30">
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing labore et dolore magna aliqua uat veniama icing elit sed do eiusmo o eiusmo Lorem ipsum dolor sit amet consectetur adipisicing laboreore magna aliqua uat veniama icing elit sed do eiusmo Lorem ipsum dolor sit amet consectetur adipisicing labore et dolore magna aliqua uat veniama icing elit sed do eiusmo Lorem ipsum dolor sit amet consectetur adipisicing labore et dolore magna aliqua uat veniama icing elit sed do eiusmo</p>
-                            <blockquote class="blockquote f-30 bg-e p-30 m-t-20 m-b-20">
-                                <p class="f-20">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-                                <footer class="blockquote-footer f-17 no-bg m-t-10">Someone famous in <cite title="Source Title">Source Title</cite> </footer>
-                            </blockquote>
+                            <p class="card-date"><?php the_time('F jS, Y'); ?></p>
+                            <p><?php
+		the_content( sprintf(
+			wp_kses(
+				/* translators: %s: Name of current post. Only visible to screen readers */
+				__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'wedding-restaurant' ),
+				array(
+					'span' => array(
+						'class' => array(),
+					),
+				)
+			),
+			get_the_title()
+		) );
+
+		wp_link_pages( array(
+			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'wedding-restaurant' ),
+			'after'  => '</div>',
+		) );
+		?></p>
+                            
