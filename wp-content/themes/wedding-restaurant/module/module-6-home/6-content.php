3b8af81936c1053b48e09c0a1f6a1b26c9f256ab
hongnga1611 <hongngattdpt@gmail.com>
2019-11-04 09:44:03 +0700

    add module 6 +7

---
+++ wp-content/themes/wedding-restaurant/module/module-6-home/6-content.php
@@ -0,0 +1,59 @@
+
+    <section class="cards">
+    <div class="container">
+        <div class="block-title">
+            <h4 class="card-subtitle">Gallery</h4>
+            <h2 class="line-default">Our Gallery</h2>
+        </div>
+        <div class="row row-15">
+            <div class="col-sm-6 col-md-4">
+                <div class="card hover-img hover-img-top-bottom">
+                    <a class="image-zoom card-img" href="img/meat-1329684_960_720.jpg"><img src="img/meat-1329684_960_720.jpg" alt="image"></a>
+                    <div class="card-img-overlay">
+                        <i class="fa fa-search-plus f-35"></i>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4">
+                <div class="card hover-img hover-img-top-bottom">
+                    <a class="image-zoom card-img" href="/stefan-johnson-124186.jpg"><img src="img/stefan-johnson-124186.jpg" alt="image"></a>
+                    <div class="card-img-overlay">
+                        <i class="fa fa-search-plus f-35"></i>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4">
+                <div class="card hover-img hover-img-top-bottom">
+                    <a class="image-zoom card-img" href="img/pizza-2000614_960_720.jpg"><img src="img/pizza-2000614_960_720.jpg" alt="image"></a>
+                    <div class="card-img-overlay">
+                        <i class="fa fa-search-plus f-35"></i>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4">
+                <div class="card hover-img hover-img-top-bottom">
+                    <a class="image-zoom card-img" href="img/restaurant-1092728_960_720.jpg"><img src="img/restaurant-1092728_960_720.jpg" alt="image"></a>
+                    <div class="card-img-overlay">
+                        <i class="fa fa-search-plus f-35"></i>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4">
+                <div class="card hover-img hover-img-top-bottom">
+                    <a class="image-zoom card-img" href="img/seafood-2187507_960_720.jpg"><img src="img/seafood-2187507_960_720.jpg" alt="image"></a>
+                    <div class="card-img-overlay">
+                        <i class="fa fa-search-plus f-35"></i>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4">
+                <div class="card hover-img hover-img-top-bottom">
+                    <a class="image-zoom card-img" href="img/restaurant-939434_960_720.jpg"><img src="img/restaurant-939434_960_720.jpg" alt="image"></a>
+                    <div class="card-img-overlay">
+                        <i class="fa fa-search-plus f-35"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
